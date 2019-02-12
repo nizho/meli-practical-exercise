4e663d66f1709e079abc3097bd6a57193b697d9d
Diego Martinez <diego.programacion@gmail.com>
2019-02-12 11:56:31 -0300

    #4 ajustes visuales

--- src/components/Item.js
+++ src/components/Item.js
@@ -17,6 +17,18 @@ class Item extends React.Component{
                             <div className='box-short-desc'><spam className='item-short-desc'>Esta es una cafetera fantastica, no lo vas a poder creer te hace un cafe que te aplaudis los pedos</spam></div>
                         </div>                     
                     </div>
+                    <div className='item-grid-container'>
+                        <div className='item'>
+                            <div className='box-img'><img src={dummyPic} className='item-img'></img></div>
+                            <div className='box-price'>
+                                <spam className='item-price'>$ 1200</spam>
+                                <img src={shipingIcon} className='shipping-icon'></img>
+                            </div>
+                            <div className='box-region'><spam className='region'>Catamarca</spam></div>                            
+                            <div className='box-short-desc'><spam className='item-short-desc'>Esta es una cafetera fantastica, no lo vas a poder creer te hace un cafe que te aplaudis los pedos</spam></div>
+                        </div>                     
+                    </div>
+                    
     
             </React.Fragment>
         ) 
