81bdd54f00ce962e015379b431729f772c52a8b2
Diego Martinez <dmartinez@quadiontech.com>
2019-02-12 12:26:47 -0300

    # fix spam por span

--- src/components/Item.js
+++ src/components/Item.js
@@ -10,22 +10,22 @@ class Item extends React.Component{
                         <div className='item'>
                             <div className='box-img'><img src={dummyPic} className='item-img'></img></div>
                             <div className='box-price'>
-                                <spam className='item-price'>$ 1200</spam>
+                                <span className='item-price'>$ 1200</span>
                                 <img src={shipingIcon} className='shipping-icon'></img>
                             </div>
-                            <div className='box-region'><spam className='region'>Catamarca</spam></div>                            
-                            <div className='box-short-desc'><spam className='item-short-desc'>Esta es una cafetera fantastica, no lo vas a poder creer te hace un cafe que te aplaudis los pedos</spam></div>
+                            <div className='box-region'><span className='region'>Catamarca</span></div>                            
+                            <div className='box-short-desc'><span className='item-short-desc'>Esta es una cafetera fantastica, no lo vas a poder creer te hace un cafe que te aplaudis los pedos</span></div>
                         </div>                     
                     </div>
                     <div className='item-grid-container'>
                         <div className='item'>
                             <div className='box-img'><img src={dummyPic} className='item-img'></img></div>
                             <div className='box-price'>
-                                <spam className='item-price'>$ 1200</spam>
+                                <span className='item-price'>$ 1200</span>
                                 <img src={shipingIcon} className='shipping-icon'></img>
                             </div>
-                            <div className='box-region'><spam className='region'>Catamarca</spam></div>                            
-                            <div className='box-short-desc'><spam className='item-short-desc'>Esta es una cafetera fantastica, no lo vas a poder creer te hace un cafe que te aplaudis los pedos</spam></div>
+                            <div className='box-region'><span className='region'>Catamarca</span></div>                            
+                            <div className='box-short-desc'><span className='item-short-desc'>Esta es una cafetera fantastica, no lo vas a poder creer te hace un cafe que te aplaudis los pedos</span></div>
                         </div>                     
                     </div>
                     
